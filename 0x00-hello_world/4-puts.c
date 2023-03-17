4f2dd725dfa396eb97cd4798a20ebdbfc8288df1
ransordd <ransfordfrimpong55@gmail.com>
2023-03-16 21:36:23 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a line with put function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}	
