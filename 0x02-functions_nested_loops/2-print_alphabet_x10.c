78b14667930407a003fe5713c5b1f44e19f713c1
ransordd <ransfordfrimpong55@gmail.com>
2023-03-21 23:01:14 -0700

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-        char ch;
+	char ch;
 	int i;
 	i = 0;
+
 	while (i < 10)
 	{
-        for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-                _putchar(ch);
+		_putchar(ch);
 	}
-        _putchar('\n');
+	_putchar('\n');
 	i++;
 	}
 }
