85edf6e974fe693f59fbebb80b1a37907544677b
ransordd <ransfordfrimpong55@gmail.com>
2023-03-28 07:20:28 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,29 @@
 #include "main.h"
-
 /**
- * print_rev - print  strings in reverse
- * @s: function parameter
- * Return: 0
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-
 void puts2(char *str)
 {
-	int i;
-	for (i = 0;  str[i] != '\0';)
-		i++;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		y++;
+		longi++;
 	}
-		_putchar('\n');
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
 }
