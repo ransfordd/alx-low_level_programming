fa4c1d48456ce995fb627f8d2a3dff735387e809
ransordd <ransfordfrimpong55@gmail.com>
2023-03-27 17:13:35 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,13 @@
  */
 int _strlen(char *s)
 {
-		int longi = 0;
+		int i;
+		int count = 0;
 
-			while (*s != '\0')
-					{
-								longi++;
-										s++;
-											}
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			count++;
+		}
 
-				return (longi);
+		return (count);
 }
