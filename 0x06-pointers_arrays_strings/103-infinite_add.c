b7982e45437414a497be5302ea7a6f5079fd8602
ransordd <ransfordfrimpong55@gmail.com>
2023-03-30 16:48:10 -0700

    task 6

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -89,22 +89,22 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
-			if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-			if (digits >= (size_r - 1))
-				return (0);
-			*(r + digits) = (temp_tot % 10) + '0';
-			digits++;
-			j--;
-			i--;
+					if (temp_tot >= 10)
+					overflow = 1;
+					else
+						overflow = 0;
+					if (digits >= (size_r - 1))
+					return (0);
+					*(r + digits) = (temp_tot % 10) + '0';
+					digits++;
+					j--;
+					i--;
 
-	}
-	if (digits == size_r)
+				}
+				if (digits == size_r)
 		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+			*(r + digits) = '\0';
+				rev_string(r);
+				return (r);
 }
 
