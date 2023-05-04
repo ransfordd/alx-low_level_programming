17e399ae99ffbeff63bcdf23d4eab0dc140a746d
ransordd <ransfordfrimpong55@gmail.com>
2023-05-04 10:24:18 -0700

    task 1-update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,45 @@
 #include "main.h"
-
 /**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
+ * _power - calculate (base and power)
+ * @base: base of the exponet
+ * @pow: power of the exponet
+ * Return: value of base and power
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: num of prented
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	unsigned long int dev, result;
+	char flag;
 
-	for (i = 63; i >= 0; i--)
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
 	{
-		current = n >> i;
-
-		if (current & 1)
+		result = n & dev;
+		if (result == dev)
 		{
-			_putchar('1');
-			count++;
+			flag = 1;
+
 		}
-		else if (count)
-			_putchar('0');
+		else if (flag == 1 || dev == 1)
+		{
+
+		}
+		dev >>= 1;
 	}
-	if (!count)
-		_putchar('0');
 }
